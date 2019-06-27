a7ecf5a59bbdee720da6fee28a8d69b23e2e969b
Justyna Skalska <jastka4@gmail.com>
2019-06-27 12:08:08 +0200

    Add simple login screen

---
+++ lib/login/login_bloc.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:jsos_helper/authentication/authentication.dart';
+import 'package:jsos_helper/authentication/user_repository.dart';
+import 'package:meta/meta.dart';
+
+import 'login.dart';
+
+class LoginBloc extends Bloc<LoginEvent, LoginState> {
+  final UserRepository userRepository;
+  final AuthenticationBloc authenticationBloc;
+
+  LoginBloc({
+    @required this.userRepository,
+    @required this.authenticationBloc,
+  })  : assert(userRepository != null),
+        assert(authenticationBloc != null);
+
+  LoginState get initialState => LoginInitial();
+
+  @override
+  Stream<LoginState> mapEventToState(LoginEvent event) async* {
+    if (event is LoginButtonPressed) {
+      yield LoginLoading();
+
+      try {
+        final token = await userRepository.authenticate(
+          username: event.username,
+          password: event.password,
+        );
+
+        authenticationBloc.dispatch(LoggedIn(token: token));
+        yield LoginInitial();
+      } catch (error) {
+        yield LoginFailure(error: error.toString());
+      }
+    }
+  }
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:jsos_helper/authentication/authentication.dart';
+import 'package:jsos_helper/authentication/user_repository.dart';
+import 'package:jsos_helper/login/login.dart';
+
+class LoginPage extends StatelessWidget {
+  final UserRepository userRepository;
+
+  LoginPage({Key key, @required this.userRepository})
+      : assert(userRepository != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login'),
+      ),
+      body: BlocProvider(
+        builder: (context) {
+          return LoginBloc(
+            authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
+            userRepository: userRepository,
+          );
+        },
+        child: LoginForm(),
+      ),
+    );
+  }
+}
